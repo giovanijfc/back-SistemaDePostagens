ce1ea700a8543f2d91227f1a3b7ff3d56a2557d1
unknown <giovanijfc@gmail.com>
2019-03-30 16:54:07 -0300

    Projeto criado, configurado com banco de teste H2

---
+++ src/main/java/com/giovanijfc/sistemadepostagens/SistemaDePostagensApplication.java
@@ -0,0 +1,13 @@
+package com.giovanijfc.sistemadepostagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePostagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePostagensApplication.class, args);
+	}
+
+}
